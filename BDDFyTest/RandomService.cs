62af9638518e855d8cf0d65b5aeebfff099ca9af
matskasc <christos.matskas@softwarelounge.co.uk>
2015-12-14 15:48:03 +0000

    First check-in
    First repo

---
+++ BDDFyTest/RandomService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDFyTest
+{
+    public class RandomService
+    {
+        private int iterations { get; set; }
+        private bool shouldRun { get; set; }
+
+        public RandomService(int iterations, bool shouldRun)
+        {
+            this.iterations = iterations;
+            this.shouldRun = shouldRun;
+        }
+
+        public bool PrintMessage (string message)
+        {
+            if(shouldRun && iterations >= 1)
+            {
+                var count = 1;
+                do
+                {
+                    Console.WriteLine("I've already done ");
+                    count++;
+                }
+                while (count <= iterations);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BDDFyTest/RandomServiceTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace BDDFyTest
+{
+    public class RandomServiceTests
+    {
+        private int iterationsToTest;
+        private bool shouldRun;
+        private RandomService testTarget;
+
+        [Fact]
+        public void RandomServiceTaskWillRun()
+        {
+            this.Given(x => x.GivenMoreThanOneIterations())
+                .When(x => x.AndTaskSetToRun())
+                .Then(x => x.ThenTheClassShouldInitializeCorrectly())
+                .And(x => x.AndTaskShouldRun())
+                .BDDfy("Results 0");
+
+        }
+
+        [Fact]
+        public void RandomServiceTaskWillRunOnce()
+        {
+            var result = this.Given(x => x.GivenOneIterationOnly())
+                .When(x => x.AndTaskSetToRun())
+                .Then(x => x.ThenTheClassShouldInitializeCorrectly())
+                .And(x => x.AndTaskShouldRun())
+                .BDDfy("Results 1");
+        }
+
+        [Fact]
+        public void RandomServiceTaskWillNotRunWhenIterationsAreLessThanOne()
+        {
+            this.Given(x => x.GivenNoIterations())
+                .When(x => x.AndTaskSetToRun())
+                .Then(x => x.ThenTheClassShouldInitializeCorrectly())
+                .And(x => x.AndTaskShouldNotRun())
+                .BDDfy("Results 2");
+        }
+
+        [Fact]
+        public void RandomServiceTaskWillNotRunWhenShouldRunSetToFalse()
+        {
+            this.Given(x => x.GivenMoreThanOneIterations())
+                .When(x => x.AddTaskIsSetToNotRun())
+                .Then(x => x.ThenTheClassShouldInitializeCorrectly())
+                .And(x => x.AndTaskShouldNotRun())
+                .BDDfy("Results 3");
+        }
+
+        private void GivenMoreThanOneIterations()
+        {
+            iterationsToTest = 3;
+        }
+
+        private void GivenOneIterationOnly()
+        {
+            iterationsToTest = 1;
+        }
+
+        private void GivenNoIterations()
+        {
+            iterationsToTest = 0;
+        }
+
+        private void AndTaskSetToRun()
+        {
+            shouldRun = true;
+        }
+
+        private void AddTaskIsSetToNotRun()
+        {
+            shouldRun = false;
+        }
+
+        private void ThenTheClassShouldInitializeCorrectly()
+        {
+            testTarget = new RandomService(iterationsToTest, shouldRun);
+            Assert.NotNull(testTarget);
+        }
+
+        private void AndTaskShouldRun()
+        {
+            var result = testTarget.PrintMessage("hello world");
+            Assert.True(result);
+        }
+
+        private void AndTaskShouldNotRun()
+        {
+            var result = testTarget.PrintMessage("hello world");
+            Assert.False(result);
+        }
+    }
+}
